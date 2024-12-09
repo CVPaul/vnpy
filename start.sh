ef8844e6502cd6b67383409513009e16b8fa886e
xqli <xianqiu_li@163.com>
2024-12-09 09:38:45 +0000

    fix the font problem

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
+# sudo apt-get update
+# sudo apt-get install build-essential
+# sudo apt-get install fonts-wqy-zenhei fonts-wqy-microhei xfonts-intl-chinese
+# sudo fc-cache -fv
 # wget -c https://repo.anaconda.com/miniconda/Miniconda3-py310_24.9.2-0-Linux-x86_64.sh
 # sh Miniconda3-py310_24.9.2-0-Linux-x86_64.sh
-sudo apt install libgl1 libegl1 websockify libxkbcommon0
-sudo bash install.sh
-bash install.sh
-pip install vnpy_ctp
-pip install vnpy_sqlite
-pip install vnpy_ctastrategy
-pip install vnpy_ctabacktester
-pip install vnpy_datamanager
-pip install vnpy_binance
-QT_QPA_PLATFORM=vnc python examples/veighna_trader/run.py &
+# sudo apt install libgl1 libegl1 websockify libxkbcommon0
+# sudo bash install.sh
+# bash install.sh
+QT_QPA_PLATFORM=vnc python examples/binance/run.py &
 # cd noVNC && ./utils/novnc_proxy --vnc localhost:5900
 
